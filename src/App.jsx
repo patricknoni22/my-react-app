14f06958fd047d415612921e96cb14ec45b9728e
patricknoni22 <okoloigwepatrick@gmail.com>
2025-11-27 12:15:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      {/* <h1 className='text-5xl text-amber-200'>tailwind</h1> */}
+     <main className='text-3xl'>
+       <img src="./src\assets\react.svg" alt="" />
+       <h1>Fun facts about React!</h1>
+       <ul className='flex flex-col items-start gap-2.5 '>
+        <li>was first release in 2013</li>
+        <li>was originally created by jordan walker</li>
+        <li>has well over 200k stars on github</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousand of enterprise apps, including mobile apps.
+        </li>
+       </ul>
+     </main>
+  
+      
+
+    </>
+  )
+}
+
+export default App
